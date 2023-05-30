a7106be2f4e9811c2f081819197f796b824689ef
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-05-30 16:31:26 +0330

    color
    color lib

---
+++ lib/color.py
@@ -0,0 +1,10 @@
+class c:
+    def Regular_Colors:
+        Black = "\e[0;30m"
+        Red = "\e[0;31m"
+        Green = "\e[0;32m" 
+        Yellow = "\e[0;33m" 
+        Blue = "\e[0;34m"
+        Purple = "\e[0;35m"
+        Cyan = "\e[0;36m"
+        White = "\e[0;37m"
