245fd32e8fca711352ae983a4eb7a241efdbab26
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-06-01 17:44:06 +0330

    install - plat cehck

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
+# BDadmehr0 - Garfox
+
 import platform
+from time import sleep
+import os
+
+install_update():
+    try:
+
+
+sys_chck():
+    os_n = platform.system()
+    print(os_n)
+
+def banner():
+    asci = '''
+ @@@@@@@ @@@  @@@ @@@@@@@       @@@@@@ @@@@@@@@@@   @@@@@@
+   @@!   @@!@!@@@   @@!        !@@     @@! @@! @@! !@@
+   @!!   @!@@!!@!   @!!         !@@!!  @!! !!@ @!@  !@@!!  | https://github.com/BDadmehr0
+   !!:   !!:  !!!   !!:            !:! !!:     !!:     !:! | TNTsms Call & SMS Spamer (IR number)
+    :    ::    :     :         ::.: :   :      :   ::.: :  | By Dadmehr with Coffee'''
+    sleep(3)
 
 
 def main():
-    def banner():
-        banner1 = '''
- @@@@@@@ @@@  @@@ @@@@@@@       @@@@@@ @@@@@@@@@@   @@@@@@
-   @@!   @@!@!@@@   @@!        !@@     @@! @@! @@! !@@    
-   @!!   @!@@!!@!   @!!         !@@!!  @!! !!@ @!@  !@@!! 
-   !!:   !!:  !!!   !!:            !:! !!:     !!:     !:!
-    :    ::    :     :         ::.: :   :      :   ::.: : 
-                                                          
-        '''
-
-
-
 
 if __name__ == "__main__":
+
+    os.system('clear')
+    menu()
     main()
