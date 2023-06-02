7469ace6efb392d44a3336c7bb7c02ce53dde108
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-06-02 20:41:04 +0330

    json

--- lib/sms.py
+++ lib/sms.py
@@ -3,19 +3,19 @@
 import requests
 import random
 from fake_useragent import UserAgent
+import json
 
 # +_+_+_+ User Agent Genrator +_+_+_+
 # ua = UserAgent()
 # user = ua.random
 # print(user)
+
 range_n_limet = 100
 
 def send(phone_number,range_n): # add prvity Change IP
     pass
 
-import json
-
 with open('./API/sms.json') as json_file:
     data = json.load(json_file)
 
-print(data)
+print(data['url'])
