56693e721376035e694dce060fdce49f474ec479
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-06-02 06:42:57 +0330

    main

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 # Dadmher - Garfox
 
+try:
+    import platform
+    import os
+    from time import sleep
+    from colorama import Fore
+    from lib import configs
+except:
+    print('Missing Library')
+
 def install():
     try:
         #install form req.txt in lib dic
         os.system('pip install -r ./lib/requirements.txt')
-        import platform
-        from time import sleep
-        import os
-        from colorama import Fore, Style
     except:
         print(Fore.RED+'Installion Failed')
 
 
-def sys_check(os_n):
+def sys_check():
     os_n = platform.system()
     if os_n == 'Linux':
         print(Fore.GREEN+'System-Guard: Cehck and accepted')
     else:
         print(Foe.RED+'System_Guard: Check and not accepted')
+        exit()
 
 def banner():
     asci = '''
@@ -30,10 +36,10 @@ def banner():
 
 
 def main():
-    pass
-if __name__ == "__main__":
-    install_update()
-    ###os.system('clear')
-    #menu()
-    #main()
+    banner = configs.banner()
 
+if __name__ == "__main__":
+    install()
+    os.system('clear')
+    sys_check()
+    main()
