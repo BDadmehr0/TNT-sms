bc718a10e4af60ee388b7564aeaec61f8fd4656f
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-06-02 05:44:28 +0330

    color

--- lib/color.py
+++ lib/color.py
@@ -1,12 +1,43 @@
-class c:
-    def regular_colors():
-        Black = "\e[0;30m"
-        Red = "\e[0;31m"
-        Green = "\e[0;32m" 
-        Yellow = "\e[0;33m" 
-        Blue = "\e[0;34m"
-        Purple = "\e[0;35m"
-        Cyan = "\e[0;36m"
-        White = "\e[0;37m"
-    def bold():
+class colors:
+        #Background
+    Bg_Black = "\e[40m"
+    Bg_Red = "\e[41m"
+    Bg_Green = "\e[42m"
+    Bg_Yellow = "\e[43m"
+    Bg_Blue = "\e[44m"
+    Bg_Purple = "\e[45m"
+    Bg_Cyan = "\e[46m"
+    Bg_White = "\e[47m"
+        # Regular
+    Black = "\e[0;30m"
+    Red = "\e[0;31m"
+    Green = "\e[0;32m" 
+    Yellow = "\e[0;33m" 
+    Blue = "\e[0;34m"
+    Purple = "\e[0;35m"
+    Cyan = "\e[0;36m"
+    White = "\e[0;37m"
 
+class styles:
+    #BOLD
+    Bold_Black = "\e[1;30m"
+    Bold_Red = "\e[1;31m"
+    Bold_Green = "\e[1;32m"
+    Bold_Yellow = "\e[1;33m"
+    Bold_Blue = "\e[1;34m"
+    Bold_Purple = "\e[1;35m"
+    Bold_Cyan = "\e[1;36m"
+    Bold_White = "\e[1;37m"
+
+    #UnderLine
+    Unline_Black = "\e[4;30m"
+    Unline_Red = "\e[4;31m"
+    Unline_Green = "\e[4;32m"
+    Unline_Yellow = "\e[4;33m"
+    Unline_Blue = "\e[4;34m"
+    Unline_Purple = "\e[4;35m"
+    Unline_Cyan = "\e[4;36m"
+    Unline_White = "\e[4;37m"
+
+    #Reset
+    Reset = "\e[0m"
