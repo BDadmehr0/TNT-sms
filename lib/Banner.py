bb1842bf7d41c35c71fa0c068ec0741fde5dd583
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-06-10 19:28:37 +0330

    rm

--- lib/Banner.py
+++ lib/Banner.py
@@ -1,9 +1,8 @@
-from time import sleep
 from colorama import Fore as c
 def banner():
 
     print(c.RED+'@@@@@@'+c.WHITE+' @@@  @@'+c.RED+' @@@@@@'+c.WHITE+'  @@@@@@ @@@@@@@@@@   @@@@@@')
     print(c.RED+'  @@! '+c.WHITE+' @@!@!@@'+c.RED+'   @@! '+c.WHITE+' !@@     @@! @@! @@! !@@')
     print(c.RED+'  @!! '+c.WHITE+' @!@@!!@'+c.RED+'   @!! '+c.WHITE+'  !@@!!  @!! !!@ @!@  !@@!!  | Github BDadmehr0')
-    print(c.RED+'  !!: '+c.WHITE+' !!:  !!'+c.RED+'   !!: '+c.WHITE+'     !:! !!:     !!:     !:! | TNTsms Call, SMS Spamer IR')
+    print(c.RED+'  !!: '+c.WHITE+' !!:  !!'+c.RED+'   !!: '+c.WHITE+'     !:! !!:     !!:     !:! | TNTsms Call & SMS Spamer IR')
     print(c.RED+'   :  '+c.WHITE+' ::    : '+c.RED+'    :  '+c.WHITE+' ::.: :   :      :   ::.: : | By BDadmehr0\n')
