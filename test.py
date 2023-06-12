e8f39344d510d437867886fefc30942de8ebaa6b
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-06-13 03:03:48 +0330

    rm

--- test.py
+++ test.py
@@ -9,4 +9,3 @@ data = {
 
 print(post(url=link, data=data))
 
-digt_countrycode = % 2 B98 & phone = 09050756226 & digits_reg_name = dadmehr & digits_process_register = 1 & sms_otp = & digits_otp_field = 1 & instance_id = 1154 b2b327c31c49e4271a4bafddd354 & optional_data = optional_data & action = digits_forms_ajax & type = register & dig_otp = otp & digits = 1 & digits_redirect_page = % 2 F % 2 Fwww.pakhsh.shop % 2 F % 3 Fpage % 3 D1 % 26 redirect_to % 3 Dhttps % 253 A % 252 F % 252 Fwww.pakhsh.shop % 252 F & digits_form = 6 c4d0ca3e6 & _wp_http_referer = % 2 F % 3 Flogin % 3 Dtrue % 26 page % 3 D1 % 26 redirect_to % 3 Dhttps % 253 A % 252 F % 252 Fwww.pakhsh.shop % 252 F & container = digits_protected & sub_action = sms_otp
