12b4386c9b9ca08ef964b0c1df236a7373745016
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-06-13 04:04:34 +0330

    versioan create fut

--- lib/Banner.py
+++ lib/Banner.py
@@ -1,19 +1,21 @@
 from colorama import Fore as c
-def banner():
+from requests import get
 
+def banner():
+    versioan = get('https://raw.githubusercontent.com/BDadmehr0/TNT-sms/main/data/.V').text
     bomb = """
-   ,--.!,  
-,d08b.-*-
-0088MM |    
-`9MMP'"""
+       ,--.!,  
+    ,d08b.-*-   
+    0088MM |    
+    `9MMP'"""
     
-#   _____ _  _ _____            
-#  |_   _| \| |_   _|___ __  ___
-#    | | | .` | | |(_-< '  \(_-<
-#    |_| |_|\_| |_|/__/_|_|_/__/
+    #   _____ _  _ _____            
+    #  |_   _| \| |_   _|___ __  ___
+    #    | | | .` | | |(_-< '  \(_-<
+    #    |_| |_|\_| |_|/__/_|_|_/__/
                               
 
     print(c.RED+' _____'+c.WHITE+' _  _ '+c.RED+'_____ '+c.WHITE+'                ,--.'+c.YELLOW+'!,  ')
     print(c.RED+'|_   _'+c.WHITE+'| \| |'+c.RED+'_   _|'+c.WHITE+'___ __  ___ '+c.BLACK+' ,d08b.'+c.YELLOW+'-'+c.RED+'*'+c.YELLOW+'-   ')
     print(c.RED+"  | | "+c.WHITE+"| .` |"+c.RED+" | |"+c.WHITE+"(_-< '  \(_-< "+c.BLACK+" 0088MM "+c.YELLOW+"|"+c.WHITE+" |TNTsms Call & SMS Spamer IR")
-    print(c.RED+'  |_| '+c.WHITE+'|_|\_|'+c.RED+' |_|'+c.WHITE+'/__/_|_|_/__/ '+c.BLACK+' `9MMP '+c.WHITE+'   |By BDadmehr0\n')
+    print(c.RED+'  |_| '+c.WHITE+'|_|\_|'+c.RED+' |_|'+c.WHITE+'/__/_|_|_/__/ '+c.BLACK+' `9MMP '+c.WHITE+'   |By BDadmehr0 & V {}\n'.format(versioan))
