4a2d4e0c546c5cff3c8b9931eb0986968b723f60
Bill <134191240+BDadmehr0@users.noreply.github.com>
2023-07-17 15:58:11 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-sudo apt install -y python3
+apt install -y python3 python-pip # If the package manager of your operating system is not compatible with Apple, you can change this line yum, dnf, pacman, pkg ...
 pip install -r ./requirements.txt
-
-script_path=$(pwd)
-tntsms_content="#!/bin/bash\n\ncd $script_path\npython3 main.py"
-
-echo -e $tntsms_content > /usr/local/bin/tntsms.sh
-chmod +x /usr/local/bin/tntsms.sh
