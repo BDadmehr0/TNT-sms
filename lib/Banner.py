3ca7d610ecfa9713826efb34b2d0017db903fd68
mr. quacke <134191240+BDadmehr0@users.noreply.github.com>
2023-07-20 00:10:44 +0330

    Banner update

--- lib/Banner.py
+++ lib/Banner.py
@@ -17,10 +17,9 @@ def banner():
    #    | | | .` | | |(_-< '  \(_-<
    #    |_| |_|\_| |_|/__/_|_|_/__/
                            
-   BOMB_SHOP_OPEN = colored("\t\t\t\t\t\t\tBomb shop open", "red", attrs=["reverse", "blink"])
 
    print(c.RED+' _____'+c.WHITE+' _  _ '+c.RED+'_____ '+c.WHITE+'                ,--.'+c.YELLOW+'!,  ')
    print(c.RED+'|_   _'+c.WHITE+'| \| |'+c.RED+'_   _|'+c.WHITE+'___ __  ___ '+c.WHITE+' ,d08b.'+c.YELLOW+'-'+c.RED+'*'+c.YELLOW+'-   ')
    print(c.RED+"  | | "+c.WHITE+"| .` |"+c.RED+" | |"+c.WHITE+"(_-< '  \(_-< "+c.WHITE+" 0088MM "+c.YELLOW+"|"+c.WHITE+" | TNTsms Call & SMS Spamer IR")
-   print(c.RED+'  |_| '+c.WHITE+'|_|\_|'+c.RED+' |_|'+c.WHITE+'/__/_|_|_/__/ '+c.WHITE+' `9MMP '+c.WHITE+'   | By BDadmehr0 & V {}\n'.format(versioan),BOMB_SHOP_OPEN)
+   print(c.RED+'  |_| '+c.WHITE+'|_|\_|'+c.RED+' |_|'+c.WHITE+'/__/_|_|_/__/ '+c.WHITE+' `9MMP '+c.WHITE+'   | By BDadmehr0 & V {}\n'.format(versioan))
    # cprint('Bomb Shop Open', 'black', 'on_light_red', ['reverse', 'blink'])
